51dd00936ba839b612d4f914c7eab699e2982f6c
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-10-08 20:07:22 -0400

    updating our app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 
 import './header.styles.scss';
 
@@ -33,4 +34,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
